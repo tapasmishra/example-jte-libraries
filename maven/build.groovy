32758743492de31311fdecab8f19b77db38347e0
Tapas Mishra <tapas.mishra@linuxfunda.com>
2019-10-30 16:09:03 +0530

    Update build.groovy

--- maven/build.groovy
+++ maven/build.groovy
@@ -2,7 +2,7 @@ void call(){
   node('PROD-LIN7') {
     def buildCommand
     if (config.buildOps) {
-      buildCommand = "clean install" + config.buildOps
+      buildCommand = "clean install " + config.buildOps
     } else {
       buildCommand = "clean install"
     }
